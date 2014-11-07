4f45e6e1926742935e9e3dc201f5b849df0eee18
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-11-07 17:13:22 +0100

    completed ex1.1

--- beans/src/main/java/org/sola/clients/beans/application/ApplicationSearchResultBean.java
+++ beans/src/main/java/org/sola/clients/beans/application/ApplicationSearchResultBean.java
@@ -47,8 +47,7 @@ public class ApplicationSearchResultBean extends ApplicationSummaryBean {
     private String serviceStatusList;
     
     private String parcel;
-    private String section;
-   
+    
     public ApplicationSearchResultBean(){
         super();
     }
@@ -61,15 +60,6 @@ public class ApplicationSearchResultBean extends ApplicationSummaryBean {
         this.parcel = parcel;
     }
 
-    public String getSection() {
-        return section;
-    }
-
-    public void setSection(String section) {
-        this.section = section;
-    }
-    
-    
     
     public String getAgent() {
         return agent;
