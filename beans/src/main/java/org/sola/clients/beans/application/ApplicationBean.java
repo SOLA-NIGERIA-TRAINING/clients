d5fd0f233a048302735260efb9b7956c722e5077
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-11-15 18:29:29 +0100

    restored section in applicationbean

--- beans/src/main/java/org/sola/clients/beans/application/ApplicationBean.java
+++ beans/src/main/java/org/sola/clients/beans/application/ApplicationBean.java
@@ -103,6 +103,15 @@ public class ApplicationBean extends ApplicationSummaryBean {
     private ApplicationStatusTypeBean statusBean;
     public boolean addedAgent = false;
     
+    private String section;
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
     
     /**
      * Default constructor to create application bean. Initializes the following list of beans which
